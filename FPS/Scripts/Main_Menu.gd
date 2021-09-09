b48e06c944ad4ba794b0160cac24e60f8021bf09
xXGamer_GodXx <87343026+MitchellNob@users.noreply.github.com>
2021-09-09 18:37:10 +1000

    Setting up for doom clone
    I have added the new scene and got rid of scenes that were not needed.

--- FPS/Scripts/Main_Menu.gd
+++ FPS/Scripts/Main_Menu.gd
@@ -5,22 +5,19 @@ var level_select_menu
 var options_menu
 
 export (String, FILE) var testing_area_scene
-export (String, FILE) var space_level_scene
-export (String, FILE) var ruins_level_scene
+export (String, FILE) var doom_clone
 
 func _ready():
 	start_menu = $Start_Menu
 	level_select_menu = $Level_Select_Menu
 	options_menu = $Options_Menu
 	$Start_Menu/Button_Start.connect("pressed", self, "start_menu_button_pressed", ["start"])
-	$Start_Menu/Button_Open_Godot.connect("pressed", self, "start_menu_button_pressed", ["open_godot"])
 	$Start_Menu/Button_Options.connect("pressed", self, "start_menu_button_pressed", ["options"])
 	$Start_Menu/Button_Quit.connect("pressed", self, "start_menu_button_pressed", ["quit"])
 	
 	$Level_Select_Menu/Button_Back.connect("pressed", self, "level_select_menu_button_pressed", ["back"])
-	$Level_Select_Menu/Button_Level_Testing_Area.connect("pressed", self, "level_select_menu_button_pressed", ["testing_scene"])
-	$Level_Select_Menu/Button_Level_Space.connect("pressed", self, "level_select_menu_button_pressed", ["space_level"])
-	$Level_Select_Menu/Button_Level_Ruins.connect("pressed", self, "level_select_menu_button_pressed", ["ruins_level"])
+	$Level_Select_Menu/Button_Level_Testing_Area.connect("pressed", self, "level_select_menu_button_pressed", ["testing_scene"]) #When the testing scene button is selected load the testing_scene script
+	$Level_Select_Menu/Button_Level_Doom_Clone.connect("pressed", self, "level_select_menu_button_pressed", ["doom_clone_scene"]) #When the doom clone  button is selected load the doom_clone_scene script
 
 	$Options_Menu/Button_Back.connect("pressed", self, "options_menu_button_pressed", ["back"])
 	$Options_Menu/Button_Fullscreen.connect("pressed", self, "options_menu_button_pressed", ["fullscreen"])
@@ -38,8 +35,6 @@ func start_menu_button_pressed(button_name):
 	if button_name == "start":
 		level_select_menu.visible = true
 		start_menu.visible = false
-	elif button_name == "open_godot":
-		OS.shell_open("https://godotengine.org/")
 	elif button_name == "options":
 		options_menu.visible = true
 		start_menu.visible = false
@@ -48,18 +43,15 @@ func start_menu_button_pressed(button_name):
 
 
 func level_select_menu_button_pressed(button_name):
-	if button_name == "back":
-		start_menu.visible = true
+	if button_name == "back": #if the back button is pressed make the start menu visible and the options menu invisible
+		start_menu.visible = true 
 		level_select_menu.visible = false
-	elif button_name == "testing_scene":
+	elif button_name == "testing_scene": #if the testing scene button is pressed than  set the mouse and joypad sensitivity and load the testing area scene
 		set_mouse_and_joypad_sensitivity()
 		get_node("/root/Globals").load_new_scene(testing_area_scene)
-	elif button_name == "space_level":
+	elif button_name == "doom_clone_scene":
 		set_mouse_and_joypad_sensitivity()
-		get_node("/root/Globals").load_new_scene(space_level_scene)
-	elif button_name == "ruins_level":
-		set_mouse_and_joypad_sensitivity()
-		get_node("/root/Globals").load_new_scene(ruins_level_scene)
+		get_node("/root/Globals").load_new_scene(doom_clone)
 
 
 func options_menu_button_pressed(button_name):
