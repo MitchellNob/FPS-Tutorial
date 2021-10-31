92aa3e8768f0b0dcaf7d0f20a34e8844d3adce58
MitchellNob <moustache.mitchell@gmail.com>
2021-11-01 02:30:19 +1100

    In the Process of adding second enemy
    I have added comments to code, I have also started working on second enemy

--- FPS/Scenes/Imp.gd
+++ FPS/Scenes/Imp.gd
@@ -5,42 +5,44 @@ extends KinematicBody
 enum {
 	IDLE, #IDLE is a keyword within the dictionary, whenever this is called it basically acts as an if statement
 	KILL #KILL is the same as IDLE
+	ALERT #This is when they are in the Vision range but not within the shoot range
 	}
 
-export var speed = 3000 #I use export here for this variable so that you can physically see the varible in the editor
+export var speed = 1500 #I use export here for this variable so that you can physically see the varible in the editor
 
 var target = null #target doesn't equal anything yet
-var hitTimer = false #hitTimer is set to false
+var shootTimer = false #hitTimer is set to false
 var timer = null #there is no timer yet
-var hit_delay = 2 #this is the delay on the hits from the enemy
-var can_hit = true #this is a boolean to see if the enemy can hit or not, if it is true then the enemy is allowed
+var shoot_delay = 2 #this is the delay on the hits from the enemy
+var can_shoot = true #this is a boolean to see if the enemy can hit or not, if it is true then the enemy is allowed
 #to hit the player
-var gruntHealth = 20#This is the current health of the grunt
+var impHealth = 40#This is the current health of the Imp
+var shot_range = true
 
 onready var globals = get_node("/root/Globals") #I call on the globals node here so that I can use the global variables later in the code,
 #I use the onready variable here so that I don't have to put it into my ready script, it will just start up when the code is started.
 onready var Raycast = $RayCast #I reference the raycast node here so that the code realizes that I have a raycast in the scene
-onready var Grunt = $Eyes #I then reference a spatial node that represents the eyes, it is used so that the grunt will look where the 
+onready var Imp = $Eyes #I then reference a spatial node that represents the eyes, it is used so that the grunt will look where the 
 #eyes are looking
 onready var state = IDLE #when the script starts the state is put automatically into idle
 
-const turnSpeed = 2 #speed that grunt turns
+const turnSpeed = 1 #speed that Imp turns
 const Height = 10 #Height of player
-const maxHealth = 20 #the Max health of the grunt
+const maxHealth = 40 #the Max health of the grunt
 
 
 func _ready(): #everything in here will occur as soon as the node thats attatched to this script starts
 	timer = Timer.new() #We create a new Timer node
 	timer.set_one_shot(true) #we make it so that when the timer hits 0 it will stop
-	timer.set_wait_time(hit_delay) #this sets the wait time in seconds, in this case it is set to the hit_delay which is 2 seconds
+	timer.set_wait_time(shoot_delay) #this sets the wait time in seconds, in this case it is set to the hit_delay which is 2 seconds
 	timer.connect("timeout", self, "on_timeout_complete") #This runs a timeout function within itself, so when the 2 second timer is done
 	#it calls the function on_timeout_complete.
 	add_child(timer) #We also need to add a child, this means that it will become a child of Grunt
-	gruntHealth = maxHealth #on ready the gruntHealth is reset to the maxHealth
+	impHealth = maxHealth #on ready the gruntHealth is reset to the maxHealth
 
 
 func on_timeout_complete(): #When the on_timeout_complete function is called
-	can_hit = true #the boolean can_hit is equal to true, this means that the enemy is able to hit me again
+	can_shoot = true #the boolean can_hit is equal to true, this means that the enemy is able to hit me again
 
 func _process(delta): #function _process means that it processes everything within here every frame, however things like movement,
 #would be uneven if it was processed every frame. delta contains the time elapsed in seconds meaning that if you were to multiply 
@@ -48,24 +50,26 @@ func _process(delta): #function _process means that it processes everything with
 	
 	match state: 
 		KILL:
-			Grunt.look_at(target.global_transform.origin + Vector3(0, Height, 0), Vector3(0, 1, 0)) #here the Eyes
+			Imp.look_at(target.global_transform.origin + Vector3(0, Height, 0), Vector3(0, 1, 0)) #here the Eyes
 			#use a function called look_at, this sets the Grunt to look at global vector 3 transform of the target, which in 
 			#this case is the player.
-			rotate_y(deg2rad(Grunt.rotation.y * turnSpeed))
+			rotate_y(deg2rad(Imp.rotation.y * turnSpeed))
 			#this rotates the rest of the grunt to where the Eyes are looking, it uses deg2rad, which converts degrees into radians
 			#There are a multitude of benefits to using radians, we rotate the Grunts y axis by the eyes y axis timesed by the turnSpeed
 			#to make the turning seem more natural and slow it down a little.
-			move_to_target(delta) #calls the move_to_target function
 			KILL() #calls the KILL function
+			move_to_target(delta) #calls the move_to_target function		
 		IDLE:
 			pass #does not use this piece of script
+		
 
 
 func _on_Vision_body_entered(body): #If the player enters the Vision area of the Grunt node then this function is activated
 	if target == null: #if the target doesn't equal anything . . .
 		if body.is_in_group("Player"): #if the body that entered the Vision area was the player . . .
 			target = body #the target is equal to the player
-			state = KILL #the state is set to KILL
+			if shot_range:
+				state = KILL #the state is set to KILL
 
 
 func _on_Vision_body_exited(body): #when the player exits the Vision area
@@ -84,18 +88,17 @@ func move_to_target(delta): #function called in the KILL state
 
 
 func KILL():
-	if Raycast.is_colliding() && can_hit: #If the raycast is colliding with something . . .
+	if Raycast.is_colliding(): #If the raycast is colliding with something . . .
 			var hit = Raycast.get_collider() #create a variable called hit, within that variable state what the raycast collided with
 			if hit.is_in_group("Player"): #if what it collided with was grouped with Player . . .
-				globals.health -= 10 #call the global script and within the global script minus the players health by 10
-				can_hit = false #can_hit is turned to false, meaning the Grunt cannot hit
+				globals.health -= 20 #call the global script and within the global script minus the players health by 10
+				can_shoot = false #can_shoot is turned to false, meaning the Grunt cannot hit
 				timer.start() #restart the timer
 
 
 func bullet_hit(damage, bullet_hit_pos): #if the grunt is hit with a bullet
-	gruntHealth -= damage #current health of grunt is minused by the guns damage
-	if gruntHealth <= 0: #if the grunts health is below 0
+	impHealth -= damage #current health of grunt is minused by the guns damage
+	if impHealth <= 0: #if the grunts health is below 0
 		queue_free() #kill the grunt
-
 
 
