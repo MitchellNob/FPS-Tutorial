88d89bd49036c9238b417d6bfa97f9731574e543
MitchellNob <moustache.mitchell@gmail.com>
2021-11-02 03:11:11 +1100

    Added comments
    Added the comments to the game

--- FPS/Scripts/Fireball.gd
+++ FPS/Scripts/Fireball.gd
@@ -1,25 +1,18 @@
 extends Spatial
 
-var speed = -70
-var damage = 15
+var speed = -70 #set the speed of the fireball to travel backwards
 
-onready var globals = get_node("/root/Globals")
+onready var globals = get_node("/root/Globals") #grab the node globals so that this script can use the global commands
 
 func _ready():
-	pass
+	pass #it doesn't use the ready script
 
-func _physics_process(delta):
-	var forward_dir = global_transform.basis.z.normalized()
-	global_translate(forward_dir * speed * delta)
-	
+func _physics_process(delta): #
+	var forward_dir = global_transform.basis.z.normalized() #stores the global transform of the z axis, it has been normalized aswell
+	global_translate(forward_dir * speed * delta) #Adds the forward direction timesed by the speed and delta to the global transform of the fireball
+	#making it move	
 
-func _on_Spatial_body_entered(body):
-	if body.is_in_group("Player"):
-		globals.health -= 20
-		print ("hit")
-
-
-func _on_Area_body_entered(body):
-	if body.is_in_group("Player"):
-		globals.health -= 20
+func _on_Area_body_entered(body): #if something enters the CollisionShape store in a variable called body . . .
+	if body.is_in_group("Player"): #if the body is also in the group Player . . .
+		globals.health -= 20 #minus 20 health from the players global health
 		print ("hit")
--- FPS/Scripts/Imp.gd
+++ FPS/Scripts/Imp.gd
@@ -21,13 +21,13 @@ var fire_ball = preload("res://Scenes/Fireball.tscn")
 onready var globals = get_node("/root/Globals") #I call on the globals node here so that I can use the global variables later in the code,
 #I use the onready variable here so that I don't have to put it into my ready script, it will just start up when the code is started.
 onready var Raycast = $RayCast #I reference the raycast node here so that the code realizes that I have a raycast in the scene
-onready var Imp = $Eyes #I then reference a spatial node that represents the eyes, it is used so that the grunt will look where the 
+onready var Imp = $Eyes #I then reference a spatial node that represents the eyes, it is used so that the Imp will look where the 
 #eyes are looking
 onready var state = IDLE #when the script starts the state is put automatically into idle
 
 const turnSpeed = 4 #speed that Imp turns
 const Height = 10 #Height of player
-const maxHealth = 100 #the Max health of the grunt
+const maxHealth = 100 #the Max health of the Imp
 onready var Impspeed = 800 #I use export here for this variable so that you can physically see the varible in the editor
 
 
@@ -37,13 +37,13 @@ func _ready(): #everything in here will occur as soon as the node thats attatche
 	timer.set_wait_time(shoot_delay) #this sets the wait time in seconds, in this case it is set to the hit_delay which is 2 seconds
 	timer.connect("timeout", self, "on_timeout_complete") #This runs a timeout function within itself, so when the 2 second timer is done
 	#it calls the function on_timeout_complete.
-	add_child(timer) #We also need to add a child, this means that it will become a child of Grunt
-	impHealth = maxHealth #on ready the gruntHealth is reset to the maxHealth
+	add_child(timer) #We also need to add a child, this means that it will become a child of Imp
+	impHealth = maxHealth #on ready the imphealth is reset to the maxHealth
 
 
 func on_timeout_complete(): #When the on_timeout_complete function is called
 	can_shoot = true #the boolean can_hit is equal to true, this means that the enemy is able to hit me again
-	Impspeed = 800
+	Impspeed = 800 #once the timer is complete and the imp is allowed to shoot again it can also move again
 
 func _process(delta): #function _process means that it processes everything within here every frame, however things like movement,
 #would be uneven if it was processed every frame. delta contains the time elapsed in seconds meaning that if you were to multiply 
@@ -52,11 +52,11 @@ func _process(delta): #function _process means that it processes everything with
 	match state: 
 		KILL:
 			Imp.look_at(target.global_transform.origin + Vector3(0, Height, 0), Vector3(0, 1, 0)) #here the Eyes
-			#use a function called look_at, this sets the Grunt to look at global vector 3 transform of the target, which in 
+			#use a function called look_at, this sets the Imp to look at global vector 3 transform of the target, which in 
 			#this case is the player.
 			rotate_y(deg2rad(Imp.rotation.y * turnSpeed))
-			#this rotates the rest of the grunt to where the Eyes are looking, it uses deg2rad, which converts degrees into radians
-			#There are a multitude of benefits to using radians, we rotate the Grunts y axis by the eyes y axis timesed by the turnSpeed
+			#this rotates the rest of the Imp to where the Eyes are looking, it uses deg2rad, which converts degrees into radians
+			#There are a multitude of benefits to using radians, we rotate the Imps y axis by the eyes y axis timesed by the turnSpeed
 			#to make the turning seem more natural and slow it down a little.
 			KILL() #calls the KILL function
 			move_to_target(delta) #calls the move_to_target function
@@ -65,11 +65,11 @@ func _process(delta): #function _process means that it processes everything with
 		
 
 
-func _on_Vision_body_entered(body): #If the player enters the Vision area of the Grunt node then this function is activated
+func _on_Vision_body_entered(body): #If the player enters the Vision area of the Imp node then this function is activated
 	if target == null: #if the target doesn't equal anything . . .
 		if body.is_in_group("Player"): #if the body that entered the Vision area was the player . . .
 			target = body #the target is equal to the player
-			if shot_range:
+			if shot_range: #if within the shot range . . .
 				state = KILL #the state is set to KILL
 
 
@@ -82,7 +82,7 @@ func _on_Vision_body_exited(body): #when the player exits the Vision area
 
 func move_to_target(delta): #function called in the KILL state
 	var direction = (target.transform.origin - transform.origin).normalized() #this gets a variable named the direction which houses the 
-	#players transform minused by the grunts origin and then the .normalized minuses that number by one
+	#players transform minused by the Imps origin and then the .normalized minuses that number by one
 	move_and_slide(direction * Impspeed * delta, Vector3.UP) #this uses move_and_slide to move the player towards the Player, it timeses the
 	#number we got just then (direction) by the speed and then delta to keep it consistent, it also moves on the UP vector, which can be 
 	#written as Vector3(0,1,0)
@@ -94,23 +94,23 @@ func KILL():
 			if hit.is_in_group("Player"): #if what it collided with was grouped with Player . . .
 				if can_shoot == true:
 					fireball() #call the fireball function
-					can_shoot = false #can_shoot is turned to false, meaning the Grunt cannot hit
+					can_shoot = false #can_shoot is turned to false, meaning the Imp cannot hit
 					timer.start() #restart the timer
-					Impspeed = 0
+					Impspeed = 0 #sets the speed to 0 to stop the imp from moving while shooting and reloading
 
 
-func bullet_hit(damage, bullet_hit_pos): #if the grunt is hit with a bullet
-	impHealth -= damage #current health of grunt is minused by the guns damage
-	if impHealth <= 0: #if the grunts health is below 0
+func bullet_hit(damage, bullet_hit_pos): #if the Imp is hit with a bullet
+	impHealth -= damage #current health of Imp is minused by the guns damage
+	if impHealth <= 0: #if the Imps health is below 0
 		globals.score + 100 #increase score by 100
 		queue_free() #kill the Imp
 
-func fireball():
-	var clone = fire_ball.instance()
-	var scene_root = get_tree().root.get_children()[0]
-	scene_root.add_child(clone)
+func fireball(): #The fireball function
+	var clone = fire_ball.instance() #it instantiates the scene fireball and stores it in a variable called clone
+	var scene_root = get_tree().root.get_children()[0] #creates another variable that stores the tree items first child
+	scene_root.add_child(clone) #creates the instantiated object and adds it as a child of the scene_root
 
-	clone.global_transform = $Eyes.global_transform
-	clone.scale = Vector3(4, 4, 4)
-
+	clone.global_transform = $Eyes.global_transform #sets the transform of the instantiated object facing to where the eyes are facing
+	clone.scale = Vector3(4, 4, 4) #scales the instantiated object by 4 in every axis
+ 
 
